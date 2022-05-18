f04f8e43342c964b0f6005b41f05dfbbe818028a
Jack Kitterhing <codingmonkeyjack@gmail.com>
2022-05-18 23:30:47 +0100

    Bump plugin version and update author

--- plugin.php
+++ plugin.php
@@ -3,9 +3,9 @@
  * Plugin Name: Gutenberg Forms - Mailchimp Add-on
  * Plugin URI: https://www.gutenbergforms.com
  * Description: This add-on connects Gutenberg Forms with Mailchimp. This allows you to send leads/subscribers to your Mailchimp list with the form submissions.
- * Author: munirkamal
- * Author URI: https://cakewp.com/
- * Version: 1.0.0
+ * Author: Tetra Soft
+ * Author URI: https://jak.dev
+ * Version: 1.0.1
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: cwp-gutenberg-forms-mailchimp
