5a5c6f06b8aa5d836fc8a8c66c25bc7efa8f3b9f
Jack Kitterhing <codingmonkeyjack@gmail.com>
2022-05-18 23:35:07 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: This add-on connects Gutenberg Forms with Mailchimp. This allows you to send leads/subscribers to your Mailchimp list with the form submissions.
  * Author: Tetra Soft
  * Author URI: https://jak.dev
- * Version: 1.0.1
+ * Version: 1.0.2
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: cwp-gutenberg-forms-mailchimp
